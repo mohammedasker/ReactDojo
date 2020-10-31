06ae7554f85270581e7572380125fb004b43759d
Mohammed Asker <zetoanime@gmail.com>
2020-10-31 10:15:03 +0300

    added buttons that shows more components & one button to refresh the page

--- src/App.js
+++ src/App.js
@@ -2,13 +2,13 @@ import React, { useState } from "react";
 import "./App.css";
 
 function App() {
-	const [showButton, setShowButton] = useState(false);
-	const onClick = () => setShowButton(true);
+	const [showText, setShowText] = useState(false);
+	const onClick = () => setShowText(true);
 
 	return (
 		<div className="App">
 			<header>
-				<div class="container">
+				<div className="container">
 					<nav>
 						<a href="/">
 							<h1>React Dojo</h1>
@@ -21,13 +21,67 @@ function App() {
 					</nav>
 				</div>
 			</header>
-			<h1>Join the Dojo and become the strongest coding warrior</h1>
+			<h1>Join the Dojo and become the next coding warrior</h1>
 			<button onClick={onClick}>Start</button>
-			{showButton ? <Text /> : null}
+			{showText ? <TextOne /> : null}
 		</div>
 	);
 }
 
-const Text = () => <div>IT'S WORKING!!!!</div>;
+const TextOne = () => {
+	const [showText, setShowText] = useState(false);
+	const onClick = () => setShowText(true);
+
+	return (
+		<div>
+			<p>Welcome to the React Dojo</p>
+			<button onClick={onClick}>Intro</button>
+			{showText ? <TextTwo /> : null}
+		</div>
+	);
+};
+
+const TextTwo = () => {
+	const [showText, setShowText] = useState(false);
+	const onClick = () => setShowText(true);
+
+	return (
+		<div>
+			<p>
+				My name is JavaScript and I'm here to teach you every skills I know to
+				help you become the strongest Coding Warrior in the world!
+			</p>
+			<button onClick={onClick}>Continue</button>
+			{showText ? <TextThree /> : null}
+		</div>
+	);
+};
+
+const TextThree = () => {
+	const [showText, setShowText] = useState(false);
+	const onClick = () => setShowText(true);
+
+	return (
+		<div>
+			<p>The training will be a living hell! Are you ready?</p>
+			<div className="choice">
+				<button onClick={onClick}>Yes</button>
+				<button onClick={onClick}>No</button>
+			</div>
+			{showText ? <TextFour /> : null}
+		</div>
+	);
+};
+
+const TextFour = () => {
+	const retryButton = () => window.location.reload();
+
+	return (
+		<div>
+			<p>I see. When you're ready for the training, let me know.</p>
+			<button onClick={retryButton}>Retry</button>
+		</div>
+	);
+};
 
 export default App;
